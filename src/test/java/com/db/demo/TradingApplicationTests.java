f106c745bfba75250c6c48e0ed92ee724ea71515
AskillTech <adroja.mansi27@gmail.com>
2023-06-26 20:25:31 +0200

    remove unused code

--- src/test/java/com/db/demo/TradingApplicationTests.java
+++ src/test/java/com/db/demo/TradingApplicationTests.java
@@ -20,8 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class TradingApplicationTests {
